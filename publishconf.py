2fc911c956f6b228d2e48724ea0f213a5d339218
Kevin Choi <thisiskevinchoi@gmail.com>
2014-12-09 02:27:30 -0800

    add site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://acousmonium.com'
+SITEURL = 'http://acousmonium.com/'
 
 THEME = '/home/kchoi/blog/pelican-bootstrap3-minimal'
 
